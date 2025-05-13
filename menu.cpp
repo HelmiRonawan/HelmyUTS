8deeaaa5d53a20840f062dbd739147d9f70d0a8e
HelmiRonawan <ahmadhelmi2804@gmail.com>
2025-05-13 18:27:50 +0800

    modifikasi menu ketiga menjadi penyortiran nilai, modifikasi judul menu dan deklarasi array

--- menu.cpp
+++ menu.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-int nilai[5];
-int n = sizeof(nilai)/sizeof(nilai[0]);
+const int n = 5;
+int nilai[n];
 
 void dMenu(){
 system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
+cout<<"Aplikasi Input Output Sorting Nilai"<<"\n";       
 cout<<"1. Masukkan Nilai"<<"\n";            
 cout<<"2. Keluaran Nilai"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
+cout<<"3. Sortirkan Nilai"<<"\n";           
 cout<<"4. Informasi"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 }
 
+//Input
 void inputNilai(){
   system("cls");
   cout << "==Input Nilai Yang Anda inginkan==" << endl << endl;
@@ -26,6 +27,7 @@ void inputNilai(){
     getch();
 }
 
+//Output
 void outputNilai(int nilai[]){
   system("cls");
   cout << "==Nilai Yang Telah Anda Masukkan==" << endl;
@@ -36,18 +38,61 @@ void outputNilai(int nilai[]){
   getch();
 }
 
+//Insertion Sort
+void sortNilai(int nilai[]){
+  system("cls");
+  char pilihan;
+  cout << "== Sorting Nilai ==" << endl;
+  cout << "Ketik 'a' untuk Ascending atau 'd' untuk Descending: ";
+  cin >> pilihan;
+
+  // Ascending
+  if (pilihan == 'a' || pilihan == 'A') {
+    for (int i = 1; i < n; i++) {
+      int key = nilai[i];
+      int j = i - 1;
+      while (j >= 0 && nilai[j] > key) {
+        nilai[j + 1] = nilai[j];
+        j--;
+      }
+      nilai[j + 1] = key;
+    }
+
+    cout << "\nNilai setelah diurutkan (Ascending): ";
+    for (int i = 0; i < n; i++) {
+      cout << nilai[i] << " ";
+    }
+
+  // Descending
+  } else if (pilihan == 'd' || pilihan == 'D') {
+    for (int i = 1; i < n; i++) {
+      int key = nilai[i];
+      int j = i - 1;
+      while (j >= 0 && nilai[j] < key) {
+        nilai[j + 1] = nilai[j];
+        j--;
+      }
+      nilai[j + 1] = key;
+    }
+
+    cout << "\nNilai setelah diurutkan (Descending): ";
+    for (int i = 0; i < n; i++) {
+      cout << nilai[i] << " ";
+    }
+
+  } else {
+    cout << "\nPilihan tidak dikenali. Silakan ketik 'a' atau 'd'.";
+  }
+
+  getch();
+}
+
+//Informasi
 void info(){
   system("cls");
   cout<<"Indonesia Hebat, Poliban Juara";
   getch();
 }
-
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
-
 
 int main() {
 
@@ -59,20 +104,16 @@ do
   switch (pl)
   {
    case '1':
-    /* code */
     inputNilai();
     break;
    case '2':
     outputNilai(nilai);
-    /* code */ 
     break;  
    case '3':
-    mPertama("ke- tiga");
-    /* code */
+    sortNilai(nilai);
     break;  
    case '4':
     info();
-    /* code */
     break;  
   case '5':
     /* code */
